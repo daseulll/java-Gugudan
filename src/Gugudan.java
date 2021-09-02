68ba5e1a042ae79e7b9eaf4a03afe686916d62d8
daseulll <nldaseul@gmail.com>
2021-09-03 07:11:01 +0900

    구구단 조건문 연습

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,15 +2,20 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-	int i = 1;
+	System.out.println("구구단 중 출력할 단은? : ");
+	Scanner scanner = new Scanner(System.in);
+	int number = scanner.nextInt();
+	scanner.close();
 	
-	while (i < 10) {
-		System.out.println( 10 * i);
-		i = i + 1;
-	}
+	System.out.println("입력한 값: " + number);  
 	
-	for (int j =1; j < 10; j++) {
-		System.out.println(11 * j);
+	if (number < 2 || number > 9) {
+		System.out.println("출력할 단은 2 - 9 사이여야 합니다. 입력한 값 : " + number);
 	}
+	else {
+		for (int i = 1; i < 10; i++) {
+			System.out.println( number * i );
+		}
+	}	
 	}
 }
