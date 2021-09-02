9bedcf2a478643840cf510fc3148ddc088faa345
daseulll <nldaseul@gmail.com>
2021-09-02 09:09:24 +0900

    반복문으로 구구단 출력하기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -2,17 +2,15 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
+	int i = 1;
+	
+	while (i < 10) {
+		System.out.println( 10 * i);
+		i = i + 1;
+	}
+	
+	for (int j =1; j < 10; j++) {
+		System.out.println(11 * j);
+	}
 	}
 }
