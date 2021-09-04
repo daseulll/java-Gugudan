0c33cbe4a212afc754d279a253348374bc31e7a9
daseulll <nldaseul@gmail.com>
2021-09-04 23:26:29 +0900

    배열에 구구단 결과값 담기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,21 +1,16 @@
-import java.util.Scanner;
-
 public class Gugudan {
 	public static void main(String[] args) {
-	System.out.println("구구단 중 출력할 단은? : ");
-	Scanner scanner = new Scanner(System.in);
-	int number = scanner.nextInt();
-	scanner.close();
-	
-	System.out.println("입력한 값: " + number);  
-	
-	if (number < 2 || number > 9) {
-		System.out.println("출력할 단은 2 - 9 사이여야 합니다. 입력한 값 : " + number);
-	}
-	else {
-		for (int i = 1; i < 10; i++) {
-			System.out.println( number * i );
+		int[] result = new int[9];
+		
+		for (int n = 2; n < 10; n++) {
+			for (int i = 0; i < result.length; i++) {
+				result[i] = n * (i + 1);
+			}
+			
+			for(int i=0; i < result.length; i++ ) {
+				System.out.println(result[i]);
+			}
+			
 		}
-	}	
 	}
 }
